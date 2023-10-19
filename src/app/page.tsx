c62970be648c395453270b26eeac1b4f01e87fc1
Cauã Fróes <bkcauafroes2012@gmail.com>
2023-10-19 14:32:03 -0300

    use client on correct places

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,15 @@
-"use client";
-import { useEffect, useState } from "react";
-import FeedCard, { IObj } from "./components/FeedCard";
 import Header from "./components/Header";
-import api from "./services/api";
 import ChatButton from "./components/ChatButton";
 import Nav from "./components/Nav";
 import Popup from "./components/Popup";
+import Feed from "./components/Feed";
 
 export default function Home() {
-  const [arr, setArr] = useState<IObj[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  async function getFeed() {
-    await api
-      .get("/products")
-      .then((res) => {
-        setArr(res.data.products);
-        setLoading(false);
-        console.log(arr);
-      })
-      .catch((e) => console.log(e));
-  }
-
-  useEffect(() => {
-    getFeed();
-  }, []);
-
   return (
     <>
       <Header />
       <Popup />
-      <div className="flex flex-col items-center justify-center mb-40 pt-24">
-        {arr?.length >= 1 && !loading ? (
-          arr.map((obj: IObj, index: number) => (
-            <FeedCard key={index} obj={obj} />
-          ))
-        ) : (
-          <></>
-        )}
-      </div>
+      <Feed />
       <Nav />
       <ChatButton />
     </>
